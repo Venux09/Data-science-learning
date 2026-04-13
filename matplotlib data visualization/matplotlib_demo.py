1236b366807ae79f20c1d08da5db4f3944b6057c
monu242 <monucut657@gmail.com>
2026-04-13 15:43:20 +0530

    add : starting to understand the data visulization using the matplotlib  library will continue doing pratise of pandas

---
+++ matplotlib data visualization/matplotlib_demo.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+mothh = [0,0,0,0,0,0,0,0,0,0,0,0,50,100,120,230,240,250,260,270]
+years = [2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025]
+plt.plot(years,mothh)
+plt.xlabel('age (in years)')
+plt.ylabel('muthh')
+plt.title('HIMESH muth year by year ')
+plt.show()
