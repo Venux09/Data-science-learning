0858fba22b4a8549270630a4729aa0a1e37e3e8d
monu242 <monucut657@gmail.com>
2026-05-02 09:40:25 +0530

    add : understood how to insert  rows in the table using the into and values and understood how insert multiple rows at once using simple commas

---
+++ sql/lecture_5.sql
@@ -0,0 +1,30 @@
+-- CREATE DATABASE schoolDb;
+
+-- USE schooldb;
+-- CREATE TABLE student (
+-- ID INT PRIMARY KEY,
+-- name VARCHAR(100),
+-- age INT,
+-- grade VARCHAR (10),
+-- date_of_birth DATE);
+-- SELECT * FROM student ;
+-- INSERT INTO student(id,name,age,grade,date_of_birth) VALUES (2 ,'Monu',17,'Twelth','2008-11-30');
+-- INSERT INTO student(id,name,age,grade,date_of_birth) VALUES (3,'MINY',18,'10TH','2007-11-23');
+-- INSERT INTO student(id,name,age,grade,date_of_birth) VALUES (4,'MIN',19,'12TH','2001-12-30');
+-- -- INSERT INTO student(id,name,grade,date_of_birth) VALUES ()
+-- -- INSERT INTO student(id,name,grade,date_of_birth) VALUES ()
+-- -- INSERT INTO student(id,name,grade,date_of_birth) VALUES ()
+-- -- INSERT INTO student(id,name,grade,date_of_birth) VALUES ()
+-- -- INSERT INTO student(id,name,grade,date_of_birth) VALUES ()
+-- -- INSERT INTO student(id,name,grade,date_of_birth) VALUES ()
+-- -- INSERT INTO student(id,name,grade,date_of_birth) VALUES ()
+--  SELECT * FROM student; 
+
+
+-- INSERT INTO student (id,name,age,grade,date_of_birth) VALUES
+-- (10,'AYESHA KHAN ',16,'10TH','2007-12-24'),
+-- (11,'monU verma',17,'12th','2008-11-30'),
+-- (13,'yash kumar',15,'9th','2009-11-30'),
+-- (12,'Hamed kumar',16,'12th','2009-11-30'),
+-- (14,'Chiku singh',18,'1st year','2007-12-4');
+SELECT * FROM student;
