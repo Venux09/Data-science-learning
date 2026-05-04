a8c940719532f92032f709b71048360fe53c6b7d
monu242 <monucut657@gmail.com>
2026-05-04 11:16:08 +0530

    add : understood someways to update the table rows using the set and where method and also how to use sets for multiple methods

---
+++ sql/lecture_8.sql
@@ -0,0 +1,5 @@
+USE schooldb;
+SELECT * FROM student;
+-- UPDATE student SET grade = 'X' WHERE grade = '10TH';
+-- UPDATE student SET age = 20 WHERE age 
+UPDATE student SET age = 19 , grade = 'X1' WHERE age  = 18 ;
