908ed50315aadf2238ecf9191e6d0784541baf90
monu242 <monucut657@gmail.com>
2026-05-05 11:15:20 +0530

    add : understanding how to work with commit and transaction and how does it help a lot with large data handing

---
+++ sql/lecture_10.sql
@@ -0,0 +1,9 @@
+-- SET autocommit = 0;
+-- SELECT *FROM students ;
+--  START TRANSACTION ;
+--  UPDATE students SET age = age-1 , class = class -1  WHERE age = age ;
+
+
+-- ROLLBACK;
+-- SELECT * FROM students;
+SET autocommit = 1;
