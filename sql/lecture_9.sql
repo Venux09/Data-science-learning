905d50eb53bdf74d329ef2f0c19019d7b2d0df37
monu242 <monucut657@gmail.com>
2026-05-05 10:06:25 +0530

    add : understood how to deleted data from the table using condition and also how to deleted the full table

---
+++ sql/lecture_9.sql
@@ -0,0 +1,6 @@
+USE practise_db;
+SELECT * FROM students;
+-- DELETE FROM students WHERE age= 19 IS NULL;
+SELECT  * FROM  students ;
+DELETE FROM students WHERE id = 44 ;
+DELETE FROM students WHERE id = 45;
