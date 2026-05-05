8ce649caf76c7db0ba347c30d074c5ba3e717a8d
monu242 <monucut657@gmail.com>
2026-05-05 11:37:53 +0530

    add : did some practise with the selection and conditioning making many conditions and applying the filters

--- data science pratise/sql_practise_6.sql
+++ data science pratise/sql_practise_6.sql
@@ -1,3 +1,11 @@
 USE practise_db;
 SELECT * FROM students;
-SELECT name FROM students;
+-- SELECT name FROM students; using select individual columns in the table
+-- SLECT id,class ,course,age FROM students WHERE age BETWEEN 10 AND 15;
+--  SELECT course FROM students WHERE class = 7;
+-- SELECT * FROM students WHERE age >12 OR id IS  NULL ;-- 
+-- SELECT * FROM students ORDER BY age asc;
+-- SELECT * FROM students ORDER BY age desc;
+-- SELECT * FROM students LIMIT 10;
+
+
