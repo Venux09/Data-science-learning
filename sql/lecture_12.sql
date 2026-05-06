2e8a9b652fb146cfccbfbf5bba00b70650adc28e
monu242 <monucut657@gmail.com>
2026-05-06 10:36:14 +0530

    ADD : UNDERSTOOD HOW TO USE CURRENT TIME AND CURRRENT DATE IN THE TABLE AND HOW TO INSERT THAT THING IN THE  TABLE

---
+++ sql/lecture_12.sql
@@ -0,0 +1,9 @@
+-- use practise_db;
+-- SHOW TABLES;
+-- SELECT * FROM students;
+-- SELECT CURRENT_DATE();
+-- SELECT CURRENT_TIME;
+-- SELECT CURRENT_TIMESTAMP;-- 
+-- ALTER TABLE students ADD COLUMN date_joined DATETIME DEFAULT(NOW());
+SELECT * FROM students;
+-- INSERT INTO students (id,age,date_joined) VALUES(51,54,CURRENT_TIME)
