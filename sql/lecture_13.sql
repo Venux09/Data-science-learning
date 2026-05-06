5b96ac4be7449b3dcc956ccf285ddbe43e1812c4
monu242 <monucut657@gmail.com>
2026-05-06 11:10:47 +0530

    add : understood some important constraints in the table like unique or primary and many other  like check and naming a constraints and also defualt

---
+++ sql/lecture_13.sql
@@ -0,0 +1,16 @@
+USE practise_db;
+-- USE practise_db;
+-- CREATE TABLE accounts (
+-- id INT PRIMARY KEY,
+-- balance DECIMAL (10,2) CHECK (balance>= 0)
+
+-- );
+-- INSERT INTO accounts VALUES (1,1);
+-- SELECT * FROM accounts;
+-- DESCRIBE accounts;
+-- CREATE TABLE college_students (
+-- roll_no INT PRIMARY KEY,
+--   age INT CONSTRAINT chk_age CHECK (age >= 5),
+--   email VARCHAR(100) UNIQUE -- 
+-- INSERT INTO college_studens VALUES (2,6,monu_verma@gmail.com);
+-- SELECT * FROM college_students;-- 
