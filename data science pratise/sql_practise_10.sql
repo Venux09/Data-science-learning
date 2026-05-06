73b981dce67796fd7ebe319f9191be994eb96ffb
monu242 <monucut657@gmail.com>
2026-05-06 11:49:26 +0530

    add : did little practise with date and time insertin in the tables

--- data science pratise/sql_practise_10.sql
+++ data science pratise/sql_practise_10.sql
@@ -7,3 +7,11 @@
 -- UPDATE students SET age = 18;
 -- UPDATE students SET class = 12;
 -- SELECT * FROM students;
+-- now starting to practise with the deleting methods of the table ;
+-- DELETE FROM students WHERE date_of_birth = date_of_birth;
+-- SELECT * FROM students ;
+SET autocommit = 0 ;
+
+START TRANSACTION ;
+-- ALTER TABLE students ADD COLUMN TIME_joined DATETIME DEFAULT(CURRENT_TIMESTAMP());
+SELECT * FROM students;
