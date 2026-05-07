1f7de6029414f92e3ca882b28a07bb300160f19a
monu242 <monucut657@gmail.com>
2026-05-07 11:39:29 +0530

    add ; starting the comibine practise of the sql data like we are creating also updating  and also checking at the same time and using the foreign key at the same time

---
+++ data science pratise/sql_practise_9.sql
@@ -0,0 +1,23 @@
+-- starting to practise the learning of the sql 
+-- CREATE DATABASE SUBJECTS ;
+-- SELECT CURRENT_TIMESTAMP();
+SET autocommit = 0;
+START TRANSACTION ;
+-- CREATE TABLE SUBJECTS (
+-- class_id INT AUTO_INCREMENT PRIMARY KEY,
+-- subjects VARCHAR (40))
+-- CREATE TABLE students(
+-- students_id INT AUTO_INCREMENT PRIMARY KEY ,
+-- students_name VARCHAR (100) NOT NULL,
+-- student_subject_id INT ,
+-- date_joined DATETIME  DEFAULT(CURRENT_TIMESTAMP()),
+-- FOREIGN KEY (students_subject_id) REFERENCES SUBJECTS(class_id)
+-- ON DELETE SET NULL
+-- ON UPDATE CASCADE 
+--  )
+-- INSERT INTO SUBJECTS(class_id,subjects) VALUES ((1,2,3),('Maths','Eng','Science'));
+INSERT INTO students (students_name,student_subject_id) VALUES 
+('ROHAN',1),
+('MOHAN',2),
+('SOHAN',3);
+
