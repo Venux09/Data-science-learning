13353a5ef0f145a8b00c7ba4a483981285f1328a
monu242 <monucut657@gmail.com>
2026-05-11 12:12:14 +0530

    add : solved 5 problem related to the builtin function of the sql

---
+++ data science pratise/sql_practise_12.sql
@@ -0,0 +1,39 @@
+--  here i will be  doing some practise with the union method and also with 
+-- CREATE TABLE employees_india(
+--     emp_id INT PRIMARY KEY,
+--     emp_name VARCHAR(50),
+--     department VARCHAR(50),
+--     salary DECIMAL(10,2),
+--     city VARCHAR(50),
+--     joining_date DATE
+-- );
+-- CREATE TABLE employees_usa(
+--     emp_id INT PRIMARY KEY,
+--     emp_name VARCHAR(50),
+--     department VARCHAR(50),
+--     salary DECIMAL(10,2),
+--     city VARCHAR(50),
+--     joining_date DATE
+-- );
+--  Inserting some data in the employees_india table 
+-- INSERT INTO employees_india VALUES
+-- (101, 'Aman', 'IT', 75000, 'Delhi', '2022-01-15'),
+-- (102, 'Riya', 'HR', 50000, 'Mumbai', '2021-03-10'),
+-- (103, 'Karan', 'Finance', 68000, 'Pune', '2020-07-19'),
+-- (104, 'Sneha', 'IT', 82000, 'Bangalore', '2019-11-25'),
+-- (105, 'Arjun', 'Marketing', 45000, 'Delhi', '2023-02-01'),
+-- (106, 'Meera', 'HR', 54000, 'Chennai', '2022-06-18');
+-- inserting some data in the employees_usa tabl
+
+-- inserting data in the employees_usa now
+-- INSERT INTO employees_usa VALUES
+-- (201, 'John', 'IT', 92000, 'New York', '2021-04-12'),
+-- (202, 'Emma', 'Finance', 87000, 'Chicago', '2020-08-30'),
+-- (203, 'Michael', 'Marketing', 61000, 'Boston', '2022-01-10'),
+-- (204, 'Sophia', 'HR', 58000, 'Seattle', '2019-05-22'),
+-- (205, 'David', 'IT', 99000, 'Austin', '2018-12-15'),
+-- (206, 'Olivia', 'Finance', 76000, 'San Francisco', '2023-03-08');
+
+
+
+SELECT * FROM THE 
