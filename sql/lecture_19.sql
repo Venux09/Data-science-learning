7ec11fcdc0f1fae199718eb0830bb1eab35be4cc
monu242 <monucut657@gmail.com>
2026-05-13 10:37:08 +0530

    add : here i practised indexes of the sql

---
+++ sql/lecture_19.sql
@@ -0,0 +1,5 @@
+-- here we are  going  to understand the topics in the sql 
+-- SELECT * FROM employees_india ;
+-- SELECT * FROM employees_india where salary = salary ;
+-- CREATE INDEX idx on employees_india(department);
+SHOW INDEX FROM employees_india;
