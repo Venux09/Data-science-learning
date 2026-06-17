bb98bcaae708049aa645dc0ac788a1893da278e6
Monu verma <monucut657@gmail.com>
2026-06-17 09:08:39 +0530

    add : understood how to use form like input from the users and the other methods like how to display the post form in the flask terminal

--- Web dev 2/main.py
+++ Web dev 2/main.py
@@ -1,10 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask import Flask
 
 app = Flask(__name__,static_folder='static files',static_url_path='/files')
 
 @app.route("/",methods = ["GET","POST"])
 def hello_world():
+    if request.method == 'POST':
+        print(request.form)
+        name = request.form['email']
+        password = request.form('password')
+        print(f"The name is {name} and the password is {password}")
+        return "<b> Thanks for using facebook ! you have logged in  "
     return render_template('index.html')
 
 
