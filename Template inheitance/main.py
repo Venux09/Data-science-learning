ead75ea62e0add3935cad4f75aad642c2eccbff5
Monu verma <monucut657@gmail.com>
2026-06-18 09:28:00 +0530

    add : understood how to use parameter to use in the paramters

--- Template inheitance/main.py
+++ Template inheitance/main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
 
 from flask import Flask
-
+ 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    name = request.args('name', default = 'Unnamed')
+    lang  = request.args('lang')
+    return render_template('index.html',lang = lang, name = name)
+
 
 app.run(debug=True)
