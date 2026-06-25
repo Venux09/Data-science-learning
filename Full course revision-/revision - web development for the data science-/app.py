4455e834b026b5a8f806e167b2f8467548c55c16
Monu Verma <monucut657@gmail.com>
2026-06-25 08:34:50 +0530

    add : commit

--- Full course revision-/revision - web development for the data science-/app.py
+++ Full course revision-/revision - web development for the data science-/app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request
 #basic minimal flask app 
-app = Flask(__name__)
+app = Flask(__name__ , static_folder='static',static_url_path="Data science learning\Full course revision-\revision - web development for the data science-\static")
 
 @app.route("/")
 def hello_world():
@@ -24,6 +24,7 @@ def prediction ():
     return f"The predicition from the input is {feature}"#returning this on the app interface 
     
 #serving the static file on the main website - css files , java files and other images and media 
+#changing the location of the static files
 
 
 app.run(debug=True)
