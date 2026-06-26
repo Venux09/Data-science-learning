8ea776a658a29be1c606a6611593708561b830d8
Monu Verma <monucut657@gmail.com>
2026-06-26 09:16:49 +0530

    add : tried the jsonfify for the api

---
+++ Revision/python.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/api/predict', methods=['GET'])
+
+
+def predict():
+    
+    
+    value = request.args.get('value')
+    
+    
+    
+    if value is None:
+        return jsonify({'error': 'Missing input'}), 400
+    # Simulated prediction (replace with actual model call)
+    result = int(value) * 2
+    
+    return jsonify({
+         
+         "input":value,
+         "prediction":result
+    })
+
+
+
+app.run(debug=True)
