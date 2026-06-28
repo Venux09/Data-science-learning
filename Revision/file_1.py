a3eb8564093ab4f705c585ff0e8cd98980dd4ae9
Monu Verma <monucut657@gmail.com>
2026-06-28 08:44:05 +0530

    add : intial commit

--- Revision/file_1.py
+++ Revision/file_1.py
@@ -9,7 +9,7 @@ def step(x):
     return 1 if x >= 0 else 0 
 
 def perceptron(x1,x2,w1,w2,b):
-    z = x1*w1 + x2*w2 +b #perceptron formula for the application of the perceptron 
+    z = x1*w1 + x2*w2 +b #perceptron formula for the application of the perceptron           
 
     return step(z) 
 
@@ -17,5 +17,7 @@ print(perceptron(0,0,1,1,-1.5))#values for the perceptron weights and inputs and
 print(perceptron(1,1,1,1,-1.5))
 print(perceptron(1,0,1,1,-1.5))
 print(perceptron(0,1,1,1,-1.5))
+
+
 
 
